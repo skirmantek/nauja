6dcef14b2e15c98a4128429bfe0394f76b686d74
skirmantek <skirmante.klimciukaite@gmail.com>
2019-10-17 18:24:00 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+entry: './src/index.js',
+output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+},
+
+module: {
+    rules: [
+        { test: /\.css$/, 
+            use:[
+                'style-loader',
+                'css-loader',
+            ]
+        }
+    ]
+}
+
+}
